12c1e33528fac611b0a84a3a9ff8ad23b42e42b6
TaegeonKim <aksen5240@gmail.com>
2025-08-03 14:50:48 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600 underline">
+        Hello Tailwind 4.1 + Vite!
+      </h1>
+      <p className="mt-4 text-gray-700">
+        This is a simple setup using Tailwind CSS with Vite and React.
+      </p>
+    </div>
+  );
+}
